560107e174a2559c98058ebd530d67805fec0b85
Sal <salem.ezz@gmail.com>
2018-08-29 13:26:24 -0400

    edit image upload test

--- routes/api/product.js
+++ routes/api/product.js
@@ -78,20 +78,6 @@ module.exports = function() {
     });
 
     Router.put('/products/:id', function(req, res) {
-                // let uploadedFile = req.body;
-                console.log("HERE");
-                console.log(req.files.image);
-                console.log(uploadedFile);
-                // uploadedFile.mv(path.join(__dirname, `/public/uploads/${uploadedFile.name}`), function(err) {
-                //     if (err) {
-                //         console.log(err);
-                //         return res.status(500).send(err);
-                //     }
-                // });
-                
-                console.log(req.files.image.path);
-                cloudinary.uploader.upload(req.files.image.path, function(result) { 
-                    req.body.image = result.url;
 
         pd.editProduct(req.connection, req.params.id, req.body)
         .then(product => res.json(product))
@@ -105,7 +91,7 @@ module.exports = function() {
                 })
             })
         });
-        });
+
 
     Router.delete('/products/:id', function(req, res) {
         console.log("test")
