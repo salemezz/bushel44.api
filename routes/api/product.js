16355c88168ae51a34b339a65b986b24f36abe35
Sal <salem.ezz@gmail.com>
2018-08-29 12:55:21 -0400

    edit image upload test

--- routes/api/product.js
+++ routes/api/product.js
@@ -1,8 +1,5 @@
-/** API User route for CRUD operations on users of the tool */
-//Node level imports
 const express = require('express')
 const middleware = require('../../libraries/middleware')
-//User level imports
 const pd = require('../../controllers/product')
 const path = require('path');
 const cloudinary = require('cloudinary');
@@ -15,9 +12,9 @@ cloudinary.config({
   });
 
 module.exports = function() {
-    //Create a Router instance, so we can mount routes on that and pass it up higher in the imports.
+
     const Router = express.Router()
-    //Get all the users.
+
     Router.get('/products', function(req, res) {
         console.log(res)
 
@@ -35,24 +32,6 @@ module.exports = function() {
                 })
             });
     })
-    // //Get a specific user by id.
-    // Router.get('/myProducts', function(req, res){
-    //     console.log(res)
-    //     console.log('test')
-    //     pd.getMyProducts(req.connection)
-    //     .then(products=>{
-    //         res.json(products)
-    //     })  
-    //     .catch(err=>{
-    //         console.log(err)
-    //         res.status(500)
-    //         res.json({
-    //             err: 'Internal Server Error',
-    //             message: 'Unable to get products.',
-    //             stack: err
-    //         })
-    //     });
-    // })
 
     Router.get('/products/:id', function(req, res) {
         pd.getProducts(req.connection, [req.params.id])
@@ -97,47 +76,19 @@ module.exports = function() {
           });
 
     });
-    //Router.post('/productImage', function(req, res){
-    //console.log('test:' + req.connection);
-    //var buffer = new Buffer(req.body,'base64');
-    //fs.writeFile('myUploadImage.png',buffer);
-    //})
-    // Router.post('/products', function (req, res) {
-    //     req.connection.product.create(req.body)
-    //         .then(newProduct => {
-    //             console.log('test:' + newProduct.id)
-    //             req.connection.user.findOne({
-    //                 where: {
-    //                     id: newProduct.id
-    //                 }
-    //             }).then((user) => {
-    //                 user.setProduct(newProduct.id)
-    //             }).then(() => {
-    //                 req.json(newProduct.dataValues)
-    //             })
-    //         })
-    //         .catch(err => {
-    //             console.log(err)
-    //             res.status(500)
-    //             res.json({
-    //                 err: 'Internal Server Error',
-    //                 message: 'Unable to create a new product.',
-    //                 stack: err
-    //             })
-    //         })
-    // })
-    //Edits an existing user. This is a protected route, so only logged in users can access this route.
+
     Router.put('/products/:id', function(req, res) {
-                let uploadedFile = req.body;
-                // console.log("HERE");
-                // console.log(req.files.image);
-                // console.log(uploadedFile);
-                uploadedFile.mv(path.join(__dirname, `/public/uploads/${uploadedFile.name}`), function(err) {
-                    if (err) {
-                        console.log(err);
-                        return res.status(500).send(err);
-                    }
-                });
+                // let uploadedFile = req.body;
+                console.log("HERE");
+                console.log(req.files.image);
+                console.log(uploadedFile);
+                // uploadedFile.mv(path.join(__dirname, `/public/uploads/${uploadedFile.name}`), function(err) {
+                //     if (err) {
+                //         console.log(err);
+                //         return res.status(500).send(err);
+                //     }
+                // });
+                
                 console.log(req.files.image.path);
                 cloudinary.uploader.upload(req.files.image.path, function(result) { 
                     req.body.image = result.url;
@@ -156,7 +107,6 @@ module.exports = function() {
         });
         });
 
-    //Deletes a user. This is a protected route, so only logged in users can access this route.
     Router.delete('/products/:id', function(req, res) {
         console.log("test")
         pd.removeProduct(req.connection, req.params.id)
